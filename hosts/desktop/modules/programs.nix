57d3caeb9a9f69387c4df5b7d9c9a0c8a1ec2184
D7TUN6 <d7tun6@gmail.com>
2025-12-21 23:33:25 +0500

    update

--- hosts/desktop/modules/programs.nix
+++ hosts/desktop/modules/programs.nix
@@ -7,43 +7,15 @@
 }: {
   programs = {
     gphoto2.enable = true;
-
-    # ssh = {
-    #   askPassword = lib.mkForce "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
-    # };
-    # gamescope = {
-    #   enable = true;
-    # };
-    # chromium = {
-    #   enable = true;
-    #   extensions = [
-    #     "eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # Dark Reader
-    #     "aapbdbdomjkkjkaonfhkkikfgjllcleb;https://clients2.google.com/service/update2/crx" # Google Translate
-    #     # "ohahllgiabjaoigichmmfljhkcfikeof;https://clients2.google.com/service/update2/crx" # AdBlocker Ultimate
-    #     # "bgnkhhnnamicmpeenaelnjfhikgbkllg;https://clients2.google.com/service/update2/crx" # Adguard Antibanner
-    #     "epcnnfbjfcgphgdmggkamkmgojdagdnn;https://clients2.google.com/service/update2/crx" # uBlock
-    #     # "bbdpgcaljkaaigfcomhidmneffjjjfgp;https://clients2.google.com/service/update2/crx" # uBlock Origin Scope
-    #     "hhdobjgopfphlmjbmnpglhfcgppchgje;https://clients2.google.com/service/update2/crx" # AdGuard VPN
-    #     # "akcocjjpkmlniicdeemdceeajlmoabhg;https://clients2.google.com/service/update2/crx" # 1VPN
-    #     # "olnbjpaejebpnokblkepbphhembdicik;https://clients2.google.com/service/update2/crx" # WebGL Fingerprint Defender
-    #     # "lanfdkkpgfjfdikkncbnojekcppdebfp;https://clients2.google.com/service/update2/crx" # Canvas Fingerprint Defender
-    #     # "nomnklagbgmgghhjidfhnoelnjfndfpd;https://clients2.google.com/service/update2/crx" # Canvas Blocker - Fingerprint Protect
-    #     "ldpochfccmkkmhdbclfhpagapcfdljkj;https://clients2.google.com/service/update2/crx" # Decentraleyes
-    #     "mlomiejdfkolichcflejclcbmpeaniij;https://clients2.google.com/service/update2/crx" # Ghostery
-    #     # "jfnangjojcioomickmmnfmiadkfhcdmd;https://clients2.google.com/service/update2/crx" # NoMiner
-    #     # "ihcjicgdanjaechkgeegckofjjedodee;https://clients2.google.com/service/update2/crx" # Malwarebytes Browser Guard
-    #     "pkehgijcmpdhfbdbbnkijodmdjhbjlgp;https://clients2.google.com/service/update2/crx" # Privacy Badger
-    #     "mnjggcdmjocbbbhaepdhchncahnbgone;https://clients2.google.com/service/update2/crx" # SponsorBlock
-    #   ];
-    # };
+    hyprland = {
+     enable = true;
+     xwayland.enable = true;
+     withUWSM = true; 
+    };
     niri.enable = true;
-    # river-classic = {
-    #   enable = true;
-    #   xwayland.enable = true;
-    # };
     java = {
       enable = true;
-      package = pkgs.javaPackages.compiler.temurin-bin.jre-21;
+      package = pkgs.javaPackages.compiler.temurin-bin.jre-25;
     };
     appimage = {
       enable = true;
@@ -55,7 +27,7 @@
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
       extraPackages = [ pkgs.jdk ];
-      # gamescopeSession.enable = true;
+      gamescopeSession.enable = true;
     };
     throne = {
       enable = true;
