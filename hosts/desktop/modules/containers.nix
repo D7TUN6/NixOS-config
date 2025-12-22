539dd4c0b05eaf8338a1181b2603a923685bd712
D7TUN6 <d7tun6@gmail.com>
2025-12-23 00:20:38 +0500

    update

--- hosts/desktop/modules/containers.nix
+++ hosts/desktop/modules/containers.nix
@@ -33,26 +33,6 @@
           };
         };
         services = {
-          dnscrypt-proxy = {
-            enable = true;
-            settings = {
-              ipv4_servers = true;
-              ipv6_servers = true;
-              dnscrypt_servers = true;
-              doh_servers = true;
-              require_nofilter = true;
-              require_dnssec = true;
-              query_log.file = "/var/log/dnscrypt-proxy/query.log";
-              sources.public-resolvers = {
-                urls = [
-                  "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-                  "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-                ];
-                cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-                minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              };
-            };
-          };
           nginx = {
             enable = true;
             virtualHosts = {
@@ -106,28 +86,6 @@
             ];
           };
         };
-        services = {
-          dnscrypt-proxy = {
-            enable = true;
-            settings = {
-              ipv4_servers = true;
-              ipv6_servers = true;
-              dnscrypt_servers = true;
-              doh_servers = true;
-              require_nofilter = true;
-              require_dnssec = true;
-              query_log.file = "/var/log/dnscrypt-proxy/query.log";
-              sources.public-resolvers = {
-                urls = [
-                  "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-                  "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-                ];
-                cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-                minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              };
-            };
-          };
-        };  
         systemd.services.minecraft-server-thecomboxmc = {
           wantedBy = [ "multi-user.target" ];
           serviceConfig = {
@@ -175,26 +133,6 @@
           };
         };
         services = {
-          dnscrypt-proxy = {
-            enable = true;
-            settings = {
-              ipv4_servers = true;
-              ipv6_servers = true;
-              dnscrypt_servers = true;
-              doh_servers = true;
-              require_nofilter = true;
-              require_dnssec = true;
-              query_log.file = "/var/log/dnscrypt-proxy/query.log";
-              sources.public-resolvers = {
-                urls = [
-                  "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-                  "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-                ];
-                cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-                minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-              };
-            };
-          };
           xray = {
             enable = true;
             settings = {
